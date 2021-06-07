b4606721c58c90dac8d662f868aa2253cbebd651
EC2 Default User <ec2-user@ip-172-31-28-141.ec2.internal>
2021-06-07 14:42:58 +0000

    [Update] 課題１途中

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+
+  def index
+      @book = Book.new
+      @books = Book.all
+  end
+
+  def show
+  end
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book= Book.new(book_params)
+    book.save
+    redirect_to books_path("book/id")
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+
+  end
+
+end

1202d8924d88a185a5d6a948028bb7e801f2a6c0
EC2 Default User <ec2-user@ip-172-31-28-141.ec2.internal>
2021-06-08 02:20:10 +0000

    [Start] 6/8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,24 @@
 class BooksController < ApplicationController
 
   def index
-      @book = Book.new
-      @books = Book.all
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
+
   def new
     @book = Book.new
   end
 
+
   def create
     book= Book.new(book_params)
     book.save
-    redirect_to books_path("book/id")
+    # book変数に格納したデータの、idカラムを使用するためbook.idを指定する。
+    redirect_to book_path(book.id)
   end
 
   def edit
